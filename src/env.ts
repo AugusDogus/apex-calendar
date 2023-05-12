97e45e9fee131984bfc09c8411eebad3d6423bac
AugusDogus <9794679+AugusDogus@users.noreply.github.com>
2023-05-12 17:07:49 -0500

    feat: add default calendar url

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,10 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    CALENDAR_URL: z.string(),
+    CALENDAR_URL: z
+      .string()
+      .optional()
+      .default('https://docs.google.com/spreadsheets/d/1Ca_ixagdxMgSn9zeT8ZBwJmgtTt9i3wR59BfV1htkPY'),
     DISCORD_TOKEN: z.string(),
     interval: z.coerce.number().int().positive().optional().default(60),
     DATABASE_PATH: z.string().optional().default('./'),
